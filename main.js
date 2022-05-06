580ec5c032a6f3e9210ea7cfc4c6aabe7af26166
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-05-06 13:01:34 +0900

    修正2

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
-console.log("JSは正常に読み込まれています。")
+
 const addBtn = document.getElementById('addBtn');
-let id_count =0;
 
 addBtn.addEventListener('click',function addValue(){
     
     const get_txt = document.getElementById('txt_data').value;
     const str_p = document.getElementById('start_point');
-    const Newli = document.createElement('li');
-    const Newbtn= document.createElement('input');
-            Newbtn.type='button';
-            Newbtn.name='name'
-            Newbtn.value='完了'
+    const newLi = document.createElement('li');
+    const newBtn= document.createElement('input');
+            newBtn.type='button';
+            newBtn.name='name'
+            newBtn.value='完了'
     
     const wrapper=document.createElement('div');
             wrapper.className='list-wrapper';
-    const process = document.createElement('input');
-            process.type='button';
-            process.name='name';
-            process.value='削除';
+    const controlBtn = document.createElement('input');
+            controlBtn.type='button';
+            controlBtn.name='name';
+            controlBtn.value='削除';
 
     const emp =document.createElement('br');
     const para= document.createElement('p')
-    
-    id_count++;
-    let object = { id:`${id_count}`,txt:`${get_txt}`}
-    
-    Object.keys(object).forEach(key => {
-
-        str_p.appendChild(Newli);
-        Newli.appendChild(para).textContent = `${id_count}\b\b\b${get_txt}`;
-        para.appendChild(wrapper);
-        wrapper.appendChild(Newbtn);
-        wrapper.appendChild(process);
+  
+    let object = [{ status:`${get_txt}`}];
+    object.push(`${get_txt}`);
+    object.forEach(function(status_o,index) {
+       index++;
+       status_o=`${index}\b\b\b${object.status}`
+            str_p.appendChild(newLi);
+          newLi.appendChild(para).textContent = status_o;
+        //   object.push(status);
+          para.appendChild(wrapper);
+        wrapper.appendChild(newBtn);
+        wrapper.appendChild(controlBtn);
 
 });
+
 });
